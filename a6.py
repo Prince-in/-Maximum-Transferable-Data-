cb1674818089e8b7df2bd081ecb273cf948dc64b
Prince-in <137891667+Prince-in@users.noreply.github.com>
2023-06-28 07:55:27 +0530

    Update a6.py

--- a6.py
+++ a6.py
@@ -1,108 +1,84 @@
-from ctypes.wintypes import tagPOINT
-from queue import Empty
-
-
-class Stack:
-    '''Lifo'''
-    class _Node:
-        __slots__ = ['_element','_next']
-        def __init__(self,element,next):
-            self._element = element
-            self._next = next
-
-    def __init__(self):
-        self._head = None
-        self._size = 0
-    def __len__(self):
-        return self._size
-    def is_empty(self):
-        return self._size == 0
-    def top(self):
-        if self.is_empty():
-            raise Empty('stack is empty')
-        return self._head._element 
-    def push(self,e):
-        self._head = self._Node(e,self._head)
-        self._size += 1
-    def pop(self):
-        if self.is_empty():
-            raise Empty('stack is empty')
-        answer = self._head._element
-        self._head = self._head._next
-        self._size -= 1
-        return answer
-    def __str__(self):
-        arr = ''
-        start = self._head 
-        for i in range(self._size):
-            arr += str(start._element) + ','
-            start = start._next
-            return '<' + arr + ']'
-S  = Stack()
-def findPositionandDistance(P):
-    s1 = Stack()
-    s2 = Stack()
-    s3 = Stack() 
-    s4 = Stack()
-    s5 = Stack()
-    s6 = []
-    i = 0
-    x = 1 
-    while i < len(P):
-        if (P[i] == "("):
-            s5.push(P[i])
-            i += 1
-            continue
-        elif (P[i] == ")"):
-            s5.pop()
-            i +=1 
-        elif P[i:i+2] == "+X":
-            s1.push(x)
-            s4.push(x)
-        elif P[i:i+2] == "-X":
-            s1.push(-x)
-            s4.push(x)
-        elif P[i:i+2] == "+Y":
-            s2.push(x)
-            s4.push(x)
-        elif P[i:i+2] == "-Y":
-            s2.push(-x)
-            s4.push(x)
-        elif P[i:i+2] == "+Z":
-            s3.push(x)
-            s4.push(x)
-        elif P[i:i+2] == "-Z":
-            s3.push(-x)
-            s4.push(x)
-        elif P[i].isnumeric():
-            j = i + 1
-            while P[j].isnumeric():
-                j += 1
-            t = eval(P[i:j])
-            if len(s6) == 0:
-                x = t
-                s6.append(t)
-            else:
-                x = t*s6[2*len(s6)-len(s5) - 1]
-                s6.append(t)
-            i = j+1
-            continue
-        i += 2
-    j,k,l,m = 0,0,0,0
-
-
-    for i in range (len(s1)):
-        j += s1._head._element
-        s1.pop()
-    for i in range (len(s2)):
-        k += s2._head._element
-        s2.pop()
-    for i in range (len(s3)):
-        l += s3._head._element
-        s3.pop()
-    for i in range (len(s4)):
-        m += s4._head._element
-        s4.pop()
-    return [j,k,l,m]
-print(findPositionandDistance("+X2(-Z-X+Y)8(+Y)9(-Z-Z)"))
-print(findPositionandDistance("1(+X)5(+Y)41(+Z)1805(-X)3263441(-Y)10650056950805(-Z)"))
+def swap(l,i,j):
+    l[i],l[j] = l[j],l[i]
+def checkparent(i):
+    if (i-1) // 2 >= 0:
+        return True
+    else:
+        return False
+def parent(i):
+    return (i-1) // 2
+def heapify(l,i):
+    max = i
+    left = 2*i + 1
+    right =  2*i + 2
+    if (left < len(l)) and (l[max][1] < l[left][1]):
+        max = left
+    if (right < len(l)) and (l[max][1] < l[right][1]):
+        max = right
+    if max != i:
+        swap(l,i,max)
+        heapify(l,max)
+    return l     
+def heap(l):
+    for i in range(len(l),-1,-1):
+        heapify(l,i)
+    return l
+def heapup(l,i):
+    if checkparent(i):
+        if l[i][1] > l[parent(i)][1]:
+            swap(l,i,parent(i))
+            return heapup(l,parent(i))
+def extractmax(l):
+    x = l[0]
+    swap(l,0,-1)
+    l.pop()
+    heapify(l,0)
+    return x
+def enqueue(l,data):
+    l.append(data)
+    heapup(l,len(l)-1)
+def graph(l,n):
+    g = []
+    for i in range(n):
+        g.append([])
+    for i in range(len(l)):
+        k = l[i][0]
+        m = l[i][1]
+        o = l[i][2]
+        g[k].append((m,o))
+        g[m].append((k,o)) 
+    return g
+def findMaxCapacity(n,links,s,t):
+    res = [float('inf'),[]]#this stores the result
+    gr = graph(links,n)
+    cap = []#this stores the max capacity to the router at the ith index
+    previous = []#this stores the previous router
+    reached = []#this ensures to not visit the same router
+    for i in range(n):
+        cap.append(-1)
+        previous.append(None)
+        reached.append(False)
+    cap[s] = float('inf')
+    l = [(s,0)]
+    while (len(l) != 0):
+        x = extractmax(l)
+        if x[1] >= cap[x[0]]:
+            cap[x[0]] = x[1]
+        reached[x[0]] = True
+        for i in range(len(gr[x[0]])):
+                k = gr[x[0]][i][0]
+                m = gr[x[0]][i][1]
+                if reached[k] == False:
+                    if min(cap[x[0]],m) > cap[k]:
+                        cap[k] = min(cap[x[0]],m)#updating the capacity
+                        previous[k] = x[0]
+                        enqueue(l,(k,min(cap[x[0]],m)))
+    i = t
+    while i != s:
+        if cap[i]<=res[0]:
+            res[0] = cap[i]
+        res[1].append(i)
+        i = previous[i]
+    res[1].append(s)
+    res[1].reverse()
+    return res
